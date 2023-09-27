e45fccb8a4b752f02a709b408b1dde4f5eab88a0
Ali Ramazan TAŞDELEN <alitasdelen@ogrenci.karabuk.edu.tr>
2023-09-27 17:50:09 +0300

    Some changes

--- src/main/java/com/restassignment/order/OrderNotFoundException.java
+++ src/main/java/com/restassignment/order/OrderNotFoundException.java
@@ -2,6 +2,6 @@ package com.restassignment.order;
 
 public class OrderNotFoundException extends RuntimeException {
     public OrderNotFoundException(Long id) {
-        super("Could not find employee " + id);
+        super("Could not find order " + id);
     }
 }
